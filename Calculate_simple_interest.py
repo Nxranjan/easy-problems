8788b1eefbb15ea4d02ef66567ad7b2c019db43d
Nrnjn <am.sc.u4cys25030@am.students.amrita.edu>
2025-08-15 14:08:37 +0530

    Delete Calculate_simple_interest.py

--- Calculate_simple_interest.py
+++
@@ -1,5 +0,0 @@
-p=int(input("Enter principal amount:"))
-r=int(input("Enter rate of interest:"))
-t=int(input("Enter time period (in months):"))
-si=(p*r*t)/100
-print("The simple interest is calculated to be",si)

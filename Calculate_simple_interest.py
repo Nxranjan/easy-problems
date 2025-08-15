294186d55b29b997c164a0518f35c714b11b7640
Nrnjn <am.sc.u4cys25030@am.students.amrita.edu>
2025-08-15 14:08:24 +0530

    Add files via upload

---
+++ Calculate_simple_interest.py
@@ -0,0 +1,5 @@
+p=int(input("Enter principal amount:"))
+r=int(input("Enter rate of interest:"))
+t=int(input("Enter time period (in months):"))
+si=(p*r*t)/100
+print("The simple interest is calculated to be",si)
